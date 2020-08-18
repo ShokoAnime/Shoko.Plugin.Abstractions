39480637a571b005e37cf4de40c4d3e389fef770
da3dsoul <da3dsoul@gmail.com>
2020-08-18 11:26:49 -0400

    Rename Shoko.Renamer.Abstractions to Shoko.Plugin.Abstractions. Start work on Plugin Settings.
    Signed-off-by: da3dsoul <da3dsoul@gmail.com>

---
+++ IRenamer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shoko.Plugin.Abstractions
+{
+    /// <summary>
+    /// A renamer must implement this to be called
+    /// </summary>
+    public interface IRenamer : IPlugin
+    {
+        void GetFilename(RenameEventArgs args);
+
+        void GetDestination(MoveEventArgs args);
+    }
+}
